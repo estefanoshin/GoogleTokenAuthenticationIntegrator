1889c78b74425fe30722c08957bec21538f83f5b
Estefano Shin <eshin@technisys.com>
2023-04-12 16:08:21 -0300

    Authenticator controller to a more standarized name

--- src/main/java/com/google/GoogleTokenAuthenticationIntegrator/controller/AuthenticatorController.java
+++ src/main/java/com/google/GoogleTokenAuthenticationIntegrator/controller/AuthenticatorController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController("/api/token")
-public class Authenticator {
+public class AuthenticatorController {
 
     /**
      * TODO:
